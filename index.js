a89bad29daf0ceeaaad6d1a17ac5871e4e9c37fc
Ludmila Alegre <ludmilaaalegre@gmail.com>
2022-10-03 00:50:44 -0300

    mejorando dom y eventos

---
+++ index.js
@@ -0,0 +1,68 @@
+function producto(id, nombre, foto, precio) {
+  this.id = id;
+  this.nombre = nombre;
+  this.foto = foto;
+  this.precio = precio;
+}
+
+var skincare = [
+  new producto(0, "Agua micelar", "images/agua-micelar.png", 2500),
+  new producto(1, "Crema hidratante", "images/crema-facial.png", 3000),
+  new producto(2, "Protector solar", "images/protector-solar.png", 2500),
+];
+
+
+let carrito = [];
+
+skincare.forEach((producto) => {
+  let productoRenderizado = document.createElement("div");
+  productoRenderizado.innerHTML = `
+<div class="col mb-5">
+<div class="card h-100">
+<!-- Product image-->
+    <img class="card-img-top" src="${producto.foto}"  alt="..." />
+<!-- Product details-->
+    <div class="card-body p-4">
+        <div class="text-center">
+<!-- Product name-->
+            <h5 class="fw-bolder">${producto.nombre}</h5>
+<!-- Product price-->
+                <p> $ ${producto.precio}</p>
+                    <button id= ${producto.id}><img  class="imagenCarrito" src="./images/agregar-carrito.png" alt="" ></button
+            </div>
+        </div>
+    </div>
+    </div>
+`;
+  content.append(productoRenderizado);
+  let boton = document.getElementById(producto.id);
+  boton.addEventListener("click", () =>
+    alert("Se agrego el producto al carrito")
+  );
+});
+const inputBuscar = document.getElementById("inputBuscar")
+const botonBuscar = document.getElementById("botonBuscar")
+botonBuscar.addEventListener("click", () => buscarProducto(inputBuscar.value));
+/* inputBuscar.addEventListener ("click", () => console.log(inputBuscar.value)); */
+
+const buscarProducto = (string) => {
+  console.log(string);
+  inputBuscar.value = ' '
+};
+
+
+
+/* const comprarProducto = (producto) => {
+    let productoExiste = carrito.find (item => item.id === producto.id)
+    if (productoExiste === undefined) {
+        carrito.push ({
+            producto: producto.nombre,
+            precio: producto.precio,
+            cantidad:1}
+        )
+    }else 
+productoExiste.precio = productoExiste.precio + producto.precio
+productoExiste.cantidad = producto.cantidad + 1}
+
+
+console.log(carrito); */
