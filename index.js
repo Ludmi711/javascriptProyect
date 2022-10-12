6e7fe376928e6fdf3798ba0ac5c7d19d63a8fd50
Ludmila Alegre <ludmilaaalegre@gmail.com>
2022-10-12 00:13:28 -0300

    agregando modal de carrito

--- index.js
+++ index.js
@@ -41,3 +41,18 @@ skincare.forEach((producto) => {
 });
 
 localStorage.setItem("carrito", JSON.stringify(skincare));
+
+
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.remove('modal--show');
+});
