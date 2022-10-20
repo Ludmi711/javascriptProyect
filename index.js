d2c866fac41cda98037b1010db81a513bf954579
Ludmila Alegre <ludmilaaalegre@gmail.com>
2022-10-19 23:37:58 -0300

    agregando fetch

--- index.js
+++ index.js
@@ -1,59 +1,50 @@
-function producto(id, nombre, foto, precio) {
-  this.id = id;
-  this.nombre = nombre;
-  this.foto = foto;
-  this.precio = precio;
-}
+fetch("./data.json")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(producto => {
+      let productoRenderizado = document.createElement("div");
+      productoRenderizado.innerHTML = `
+  <div class="col mb-5">
+  <div class="card h-100">
+  <!-- Product image-->
+      <img class="card-img-top" src="${producto.foto}"  alt="..." />
+  <!-- Product details-->
+      <div class="card-body p-4">
+          <div class="text-center">
+  <!-- Product name-->
+              <h5 class="fw-bolder">${producto.nombre}</h5>
+  <!-- Product price-->
+                  <p> $ ${producto.precio}</p>
+                      <button id= "${producto.id}" ><img src="./images/agregar-carrito.png" alt="" height="40px"></button>
+              </div>
+          </div>
+      </div>
+      </div>
+  `;
+      content.append(productoRenderizado);
+    })
 
-var skincare = [
-  new producto(0, "Agua micelar", "images/agua-micelar.png", 2500),
-  new producto(1, "Crema hidratante", "images/crema-facial.png", 3000),
-  new producto(2, "Protector solar", "images/protector-solar.png", 2500),
-];
 
-let carrito = [];
 
-skincare.forEach((producto) => {
-  let productoRenderizado = document.createElement("div");
-  productoRenderizado.innerHTML = `
-<div class="col mb-5">
-<div class="card h-100">
-<!-- Product image-->
-    <img class="card-img-top" src="${producto.foto}"  alt="..." />
-<!-- Product details-->
-    <div class="card-body p-4">
-        <div class="text-center">
-<!-- Product name-->
-            <h5 class="fw-bolder">${producto.nombre}</h5>
-<!-- Product price-->
-                <p> $ ${producto.precio}</p>
-                    <button id= ${producto.id}><img  class="imagenCarrito" src="./images/agregar-carrito.png" alt="" ></button
-            </div>
-        </div>
-    </div>
-    </div>
-`;
-  content.append(productoRenderizado);
-  let boton = document.getElementById(producto.id);
-  boton.addEventListener("click", () => {
-    Swal.fire(
-      '',
-      'Se agrego el producto al carrito',
-      'success'
-    );
+
+
+
+    let boton = document.getElementById("producto.id");
+    boton.addEventListener("click", () => {
+      Swal.fire(
+        '',
+        'Se agrego el producto al carrito',
+        'success'
+      );
+    });
   });
-});
-
-localStorage.setItem("carrito", JSON.stringify(skincare));
 
 
 let botonCarrito = document.getElementById("changuito");
-
 botonCarrito.addEventListener("click", () => {
   Swal.fire(
     'Excelente!',
     'Por ahora tenes los siguientes productos: ',
-
   );
 });
 
