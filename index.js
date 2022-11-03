574e64ad7687cb3a4af399c6076d3e108e1a8d44
Ludmila Alegre <ludmilaaalegre@gmail.com>
2022-11-02 23:03:13 -0300

    acomodando con fetch

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ fetch("./data.json")
               <h5 class="fw-bolder">${producto.nombre}</h5>
   <!-- Product price-->
                   <p> $ ${producto.precio}</p>
-                      <button id= "${producto.id}" ><img src="./images/agregar-carrito.png" alt="" height="40px"></button>
+                      <button id= "${producto.id}" class="botonci"><img src="./images/agregar-carrito.png" alt="" height="40px"></button>
               </div>
           </div>
       </div>
@@ -26,18 +26,20 @@ fetch("./data.json")
 
 
 
+
     let carrito = []
 
 
-    let boton = document.getElementById("producto.id");
-    boton.addEventListener("click", () => {
-      Swal.fire(
-        '',
-        'Se agrego el producto al carrito',
-        'success'
-      );
+    let boton = document.querySelectorAll('.botonci').forEach(boton => {
+      boton.addEventListener('click', () => {
+        Swal.fire(
+          '',
+          'Se agrego el producto al carrito',
+          'success',
+        );
+      });
     });
-  });
+  })
 
 
 let botonCarrito = document.getElementById("changuito");
@@ -48,17 +50,4 @@ botonCarrito.addEventListener("click", () => {
   );
 });
 
-/* 
-const openModal = document.querySelector('.hero__cta');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
 
-openModal.addEventListener('click', (e) => {
-  e.preventDefault();
-  modal.classList.add('modal--show');
-});
-
-closeModal.addEventListener('click', (e) => {
-  e.preventDefault();
-  modal.classList.remove('modal--show');
-}); */
